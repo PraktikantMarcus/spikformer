fdb9cdfc129c3feb86cc5d5b37435ecab0743a0e
PraktikantMarcus <Marcus.Fritzsche1@gmail.com>
2026-05-16 21:13:47 +0200

    smol fix

--- cifar10dvs/train.py
+++ cifar10dvs/train.py
@@ -379,7 +379,7 @@ def main(args):
     lr_scheduler, num_epochs = create_scheduler(args, optimizer)
     model_without_ddp = model
     if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
         model_without_ddp = model.module
 
     if args.resume:
